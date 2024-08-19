b08f56373cba4133decaa0f3cb65961d6d9db9a7
mchxo <michaelxiao1999@gmail.com>
2024-08-19 04:45:08 -0700

    batch fix typos

--- _build/jupyter_execute/content/Chapter_12/00_Standard_Deviation.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# # Standard Deviation #
-
-# The expected value $\mu_X$ of a random variable $X$ is a measure of the center of the distribution of $X$. But we know that $X$ need not be equal to $\mu_X$; indeed, $\mu_X$ need not even be a possible value of $X$.
-# 
-# How far from $\mu_X$ can $X$ be? This chapter develops an answer to that question, by examining the *mean squared error* familiar to you from Data 8.
--- _build/jupyter_execute/content/Chapter_13/00_Variance_Via_Covariance.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# # Variance Via Covariance #
-
-# Additivity is a principal property of expectation. We have used it frequently to find expectation by writing the random variable as a sum of random variables whose expectation we know or can calculate. In this chapter we will study the variance of a sum of random variables. Unlike expectation, variance is not always additive. To deal with this, we will introduce the concept of *covariance*.
-# 
-# It is worth taking some time to understand how sample sums behave, because many interesting random variables like counts of successes can be written as sums. Binomial and hypergeometric random variables are such sums. Also, the mean of a random sample is a straightforward function of the sample sum, and you have seen that random sample means are good estimators of population means. 
